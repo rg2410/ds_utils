c4ad48710aa6dce5006f22e1be29003a755f2bb4
rg2410 <rafaelgomesfernandes@hotmail.com>
2018-11-19 15:10:59 -0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='ds_utils',
       url='https://github.com/rg2410/ds_utils',
       author='Rafael Fernandes',
       author_email='rafaelgomesfernandes@hotmail.com',
-      packages=['data_analysis', 'time_series'],
+      packages=['ds_utils'],
       )
